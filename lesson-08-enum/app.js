88baf9964f4786acddb7f2152c68aa8baa01879c
J <e.orlovskiy1996@gmail.com>
2023-11-19 12:43:03 +0300

    add lesson-08-enum

---
+++ lesson-08-enum/app.js
@@ -0,0 +1,12 @@
+'use strict'
+var Direction
+;(function (Direction) {
+	Direction[(Direction['Up'] = 0)] = 'Up'
+	Direction[(Direction['Down'] = 1)] = 'Down'
+	Direction[(Direction['Left'] = 2)] = 'Left'
+	Direction[(Direction['Right'] = 3)] = 'Right'
+})(Direction || (Direction = {}))
+function getDirection(direction) {
+	return Direction[direction] || 'Unknown'
+}
+console.log(getDirection(1))
