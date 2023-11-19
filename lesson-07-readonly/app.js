3bb38614a81a79075ce95acace0e3686624e1bb7
J <e.orlovskiy1996@gmail.com>
2023-11-19 12:42:48 +0300

    add lesson-07-readonly

---
+++ lesson-07-readonly/app.js
@@ -0,0 +1,7 @@
+'use strict'
+const readOnlyArray = [1, 2, 3]
+const readOnlyTuple = ['hello', 42]
+let readOnlyObject = {
+	key: 'example',
+	value: 42
+}
---
+++ lesson-07-readonly/app.ts
@@ -0,0 +1,8 @@
+const readOnlyArray: readonly number[] = [1, 2, 3]
+
+const readOnlyTuple: readonly [string, number] = ['hello', 42]
+
+let readOnlyObject: { readonly key: string; readonly value: number } = {
+	key: 'example',
+	value: 42
+}
