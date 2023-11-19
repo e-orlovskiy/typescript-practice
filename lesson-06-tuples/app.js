2122b32b7944e577fb7048ecbd22287739027a73
J <e.orlovskiy1996@gmail.com>
2023-11-19 12:42:34 +0300

    add lesson-06-tuples

---
+++ lesson-06-tuples/app.js
@@ -0,0 +1,6 @@
+"use strict";
+const user = [1, 'Иван', 'Иванов'];
+const [id, firstname, surname] = user;
+console.log(id); // 1
+console.log(firstname); // Иван
+console.log(surname); // Иванов
---
+++ lesson-06-tuples/app.ts
@@ -0,0 +1,4 @@
+const user: [number, string, string] = [1, 'Иван', 'Иванов']
+const [id, firstname, surname] = user
+
+const arr: [number, string, ...boolean[]] = [1, 'ff', true, false, true, true, true]
