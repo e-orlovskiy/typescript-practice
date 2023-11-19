bbe751138e0bf7fe8fc2730c930da2f5bd52255d
J <e.orlovskiy1996@gmail.com>
2023-11-19 12:03:16 +0300

    add lesson-01-intro

---
+++ lesson-01-intro/app.js
@@ -0,0 +1,8 @@
+"use strict";
+let a = 5;
+let b = -5;
+console.log(a + b);
+function addMoney(sum, money) {
+    return money + sum;
+}
+addMoney(0, 10);
---
+++ lesson-01-intro/app.ts
@@ -0,0 +1,10 @@
+let a: number = 5
+let b: number = -5
+
+console.log(a + b)
+
+function addMoney(sum: number, money: number) {
+	return money + sum
+}
+
+addMoney(0, 10)
