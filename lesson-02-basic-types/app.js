ae7224fc40aafbeaa42ed9401ec346b8e237d0ca
J <e.orlovskiy1996@gmail.com>
2023-11-19 12:41:08 +0300

    add lesson-02-basic-types

---
+++ lesson-02-basic-types/app.js
@@ -0,0 +1,13 @@
+"use strict";
+let n1 = 5;
+let n2 = 10;
+let s1 = 'строка1';
+let s2 = 'строка2';
+let b1 = true;
+let b2 = false;
+let res1 = n1 + n2;
+let res2 = s1 + s2;
+let res3 = b1 && b2;
+console.log(res1);
+console.log(res2);
+console.log(res3);
---
+++ lesson-02-basic-types/app.ts
@@ -0,0 +1,16 @@
+let n1: number = 5
+let n2: number | null = 10 // union тип null
+
+let s1: string = 'строка1'
+let s2: string = 'строка2'
+
+let b1: boolean = true
+let b2: boolean = false
+
+let res1: number = n1 + n2
+let res2: string = s1 + s2
+let res3: boolean = b1 && b2
+
+console.log(res1)
+console.log(res2)
+console.log(res3)
